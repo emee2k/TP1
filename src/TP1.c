7bea228b4f2bb4b0c5b11a03bf98207d5b7651c7
Emiliano Gasparovic <emilianit2000@gmail.com>
2018-10-11 19:53:27 -0300

    Corregido TP1_6

--- src/TP1.c
+++ src/TP1.c
@@ -45,6 +45,8 @@
 #define TP1_2				(2)
 #define TP1_3				(3)
 #define TP1_4				(4)
+#define TP1_5				(5)
+#define TP1_6				(6)
 #define TICKRATE_1MS		(1)
 #define TICKRATE_10MS		(10)
 #define TICKRATE_100MS		(100)
@@ -52,7 +54,7 @@
 #define LED_TOGGLE_500MS	(500)
 #define LED_TOGGLE_1000MS	(1000)
 
-#define TEST (TP1_4)
+#define TEST (TP1_6)
 #define TICKRATE_MS (TICKRATE_10MS)
 #define LED_TOGGLE_MS (LED_TOGGLE_500MS/TICKRATE_MS)
 /*==================[internal data declaration]==============================*/
@@ -233,6 +235,7 @@ int main(void){
 	   				if (LED_Toggle_Number == 0 || LED_Toggle_Number == 1){
 	   					LED_Toggle_Counter = LED_TOGGLE_MS;
 	   					gpioToggle(LED3);
+
 	   				}
 	   				if (LED_Toggle_Number == 2 || LED_Toggle_Number == 3){
 	   					LED_Toggle_Counter = LED_TOGGLE_MS;
@@ -267,6 +270,146 @@ int main(void){
 
 	   	/* NO DEBE LLEGAR NUNCA AQUI, debido a que a este programa no es llamado
 	          por ningun S.O. */
+
+
+#endif
+
+#if (TEST == TP1_5)
+
+	   	/* ------------- INICIALIZACIONES ------------- */
+	   	uint32_t LED_Toggle_Counter = 0;
+	   	uint32_t LED_Toggle_Number = 0;
+
+	   	/* Inicializar la placa */
+	   	boardConfig();
+
+	   	/* Inicializar el conteo de Ticks con resolucion de 1ms (se ejecuta
+	          periodicamente una interrupcion cada TICKRATE_MS que incrementa un contador de
+	          Ticks obteniendose una base de tiempos). */
+	   	tickConfig( TICKRATE_MS );
+
+	   	/* Se agrega ademas un "tick hook" nombrado myTickHook. El tick hook es
+	          simplemente una funcion que se ejecutara peri�odicamente con cada
+	          interrupcion de Tick, este nombre se refiere a una funcion "enganchada"
+	          a una interrupcion.
+	          El segundo parametro es el parametro que recibe la funcion myTickHook
+	          al ejecutarse. En este ejemplo se utiliza para pasarle el led a titilar.
+	   	*/
+	   	tickCallbackSet( myTickHook, (void*)NULL );
+
+	   	/* The DEBUG* functions are sAPI debug print functions.
+	   	Code that uses the DEBUG* functions will have their I/O routed to
+	   	the sAPI DEBUG UART. */
+	   	DEBUG_PRINT_ENABLE;
+	   	/* UART for debug messages. */
+	   	debugPrintConfigUart( UART_USB, 115200 );
+	   	debugPrintString( "DEBUG c/sAPI\r\n" );
+	   	debugPrintString( "LED Toggle\n" );
+
+	   	/* ------------- REPETIR POR SIEMPRE ------------- */
+	   	while(1) {
+	   		__WFI();
+
+	   		if (LED_Time_Flag == true) {
+	   			LED_Time_Flag = false;
+
+	   			if (LED_Toggle_Counter == 0) {
+	   				if (LED_Toggle_Number == 0 || LED_Toggle_Number == 1){
+	   					LED_Toggle_Counter = LED_TOGGLE_MS;
+	   					gpioToggle(LED3);
+	   					debugPrintString( "LED Toggle\n Led 3" );
+	   				}
+	   				if (LED_Toggle_Number == 2 || LED_Toggle_Number == 3){
+	   					LED_Toggle_Counter = LED_TOGGLE_MS;
+	   					gpioToggle(LED2);
+	   					debugPrintString( "LED Toggle\n Led 2" );
+	   				}
+	   				if (LED_Toggle_Number == 4 || LED_Toggle_Number == 5){
+	   					LED_Toggle_Counter = LED_TOGGLE_MS;
+	   					gpioToggle(LED1);
+	   					debugPrintString( "LED Toggle\n Led 1" );
+	   				}
+	   				if (LED_Toggle_Number == 6 || LED_Toggle_Number == 7){
+	   					LED_Toggle_Counter = LED_TOGGLE_MS;
+	   					gpioToggle(LEDR);
+	   					debugPrintString( "LED Toggle\n Led R" );
+	   				}
+	   				if (LED_Toggle_Number == 8 || LED_Toggle_Number == 9){
+	   					LED_Toggle_Counter = LED_TOGGLE_MS;
+	   					gpioToggle(LEDG);
+	   					debugPrintString( "LED Toggle\n Led G" );
+	   				}
+	   				if (LED_Toggle_Number == 10 || LED_Toggle_Number == 11){
+	   					LED_Toggle_Counter = LED_TOGGLE_MS;
+	   					gpioToggle(LEDB);
+	   					debugPrintString( "LED Toggle\n Led B" );
+	   				}
+	   				LED_Toggle_Number++;
+	   				if (LED_Toggle_Number==12){
+	   					LED_Toggle_Number=0;
+	   				}
+	   			}
+	   			else
+	   				LED_Toggle_Counter--;
+
+	   		}
+	   	}
+
+	   	/* NO DEBE LLEGAR NUNCA AQUI, debido a que a este programa no es llamado
+	          por ningun S.O. */
+
+#endif
+
+#if (TEST == TP1_6)
+
+	   	/* ------------- INICIALIZACIONES ------------- */
+	   	uint32_t LED_Toggle_Number = 0;
+
+	   	/* Inicializar la placa */
+	   	boardConfig();
+
+	   	/* ------------- REPETIR POR SIEMPRE ------------- */
+	   	while(1) {
+	   		__WFI();
+
+	   		while(gpioRead( TEC1 )){
+	   			delay(100);
+	   		}
+
+	   		while(!gpioRead( TEC1 )){
+	   			delay(100);
+	   		}
+
+	   		LED_Toggle_Number++;
+
+	   		if (LED_Toggle_Number == 0 || LED_Toggle_Number == 1){
+	   			gpioToggle(LED3);
+
+	   		}
+	   		if (LED_Toggle_Number == 2 || LED_Toggle_Number == 3){
+	   			gpioToggle(LED2);
+	   		}
+	   		if (LED_Toggle_Number == 4 || LED_Toggle_Number == 5){
+	   			gpioToggle(LED1);
+	   		}
+	   		if (LED_Toggle_Number == 6 || LED_Toggle_Number == 7){
+	   			gpioToggle(LEDR);
+	   		}
+	   		if (LED_Toggle_Number == 8 || LED_Toggle_Number == 9){
+	   			gpioToggle(LEDG);
+	   		}
+	   		if (LED_Toggle_Number == 10 || LED_Toggle_Number == 11){
+	   			gpioToggle(LEDB);
+	   		}
+	   		LED_Toggle_Number++;
+	   		if (LED_Toggle_Number==12){
+	   			LED_Toggle_Number=0;
+	   		}
+
+	   	}
+
+	   	/* NO DEBE LLEGAR NUNCA AQUI, debido a que a este programa no es llamado
+		          por ningun S.O. */
 
 
 #endif
